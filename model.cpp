1a441262812e20a38aed547b1b591e7208ae89fb
Aaron Yang <2aronyang@gmail.com>
2023-04-08 21:09:10 -0400

    Previous progress. Should work for any length observation and any number of states.

---
+++ model.cpp
@@ -0,0 +1,20 @@
+#include <array>
+#include <cmath>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <vector>
+#define ll long long
+using namespace std;
+ifstream fin ("test.in");
+ofstream fout ("test.out");
+
+int main(){
+    
+}
